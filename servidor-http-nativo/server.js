6334f35f37b8557e8f238b577447356748e14c14
FelipeSantos067 <felipeeduardoas09@gmail.com>
2026-08-20 17:57:27 +0000

    pronto

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,20 +1,68 @@
-const http = require('node:http')
+const http = require("node:http");
 
-const porta = 3000
+const port = 3000;
 
-const server = http.createServer();
+const contatoObj = {
+	data: {
+		numero_telefone: "67 99999-9999",
+		endereco_email: "alo@gmail.com",
+	}
+}
 
-server.on('request', (req, res ) => {
-    console.log(`Requisição recebida! ${req.method} ${req.url}`);
-    console.log( `new Date().toISOString()`)
+const produtosLista = [
+	{
+		data: [
+			{id: 1, nome: "RTX 4090 ti"},
+			{id: 2, nome: "Pastilhas"},
+			{id: 3, nome: "Capacete de Moto"}
+		]
+	}
+]
 
-    res.statusCode = 201
-    res.setHeader('content-type', 'application/json; charset=utf-8');
-   
+const server = http.createServer((req, res) => {
+	
+  console.log("Cabeçalhos da 'req' (Requisição)", req.headers);
+  
+  // Cabeçalho de resposta
+  res.setHeader('Content-Type', 'application/json');
+  
+  if (req.url == "/" && req.method == "GET") {
+	  // Informações da Requisição
+	  console.log(`Método: ${req.method} e URL: ${req.url}`);
+	  // Definiu o código de status
+	  res.statusCode = 200;
+	  res.end(JSON.stringify({data: "Página Inicial"}));
+  } else if (req.url == "/contato" && req.method == "GET") {
+	  // Informações da Requisição
+	  console.log(`Método: ${req.method} e URL: ${req.url}`);
+	  // Definiu o código de status
+	  res.statusCode = 200;
+	  // Mensagem enviada
+	  res.end(JSON.stringify(contatoObj));
+  } else if (req.url == "/produtos" && req.method == "GET") {
+	  // Informações da Requisição
+	  console.log(`Método: ${req.method} e URL: ${req.url}`);
+	  // Definiu o código de status
+	  res.statusCode = 200;
+	  // Mensagem enviada
+	  res.end(JSON.stringify(produtosLista));
+  } else if (req.url == "/status" && req.method == "GET") {
+	  // Informações da Requisição
+	  console.log(`Método: ${req.method} e URL: ${req.url}`);
+	  // Definiu o código de status
+	  res.statusCode = 200;
+	  // Mensagem enviada
+	  res.end(JSON.stringify({data: {status: "ok"}}));
+  } else {
+	// Informações da Requisição
+	console.log(`Método: ${req.method} e URL: ${req.url} Não existente.`);
+	// Definiu o código de status
+	res.statusCode = 404;  
+	// Mensagem enviada
+	res.end(JSON.stringify({"error": "Página não encontrada."}));
+  }
 });
 
-server.listen(porta, ()=> {
-    console.log("Seridor ouvindo na porta ${porta}")
+server.listen(port, () => {
+    console.log(`Servidor escutando na porta ${port}`);
 });
-
-//Questão 4) A página demorou muito para carregar e depois apareceu uma msg de que a pagina nao esta funcionando
